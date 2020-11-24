a3892beaeef32030af0d177f9c8e579cdd651667
kamortech <32922387+kamortech@users.noreply.github.com>
2020-11-24 16:07:00 +0100

    Quest System Improvement
    Fisrt Quest is working properly at this moment. Conditions from last push were correct. I've just accidently added sign ';' after if statement.

--- Game/Assets/Scripts/Quests/SampleQuest1.cs
+++ Game/Assets/Scripts/Quests/SampleQuest1.cs
@@ -6,10 +6,25 @@ public class SampleQuest1 : MonoBehaviour
 {
     //must be automatic in future
     public GameObject player;
+    public GameObject questFinishObject;
 
     float distX;
     float distY;
     float distZ;
+
+    float distX_starter;
+    float distY_starter;
+    float distZ_starter;
+
+    float distX_finisher;
+    float distY_finisher;
+    float distZ_finisher;
+
+    //some variables for purposes of this excact task;
+    bool goneAwayFromEmployer;
+    bool wentToFinishingObject;
+
+    float interactionDistance;
 
     //must consider to use connection from QuestManager's definition of this struct
     struct QuestParameters
@@ -40,6 +55,10 @@ public class SampleQuest1 : MonoBehaviour
         qp.isQuestSuccess = false;
         qp.isQuestFailure = false;
 
+        interactionDistance = 1.0f;
+        goneAwayFromEmployer = false;
+        wentToFinishingObject = false;
+
         if (qp.isQuestAvailable && qp.isQuestReadyToActive)
         {
             Debug.Log("Quest just ready to active");
@@ -53,21 +72,55 @@ public class SampleQuest1 : MonoBehaviour
         {
             if (!(qp.isQuestFailure))
             {
-
-                distX = Mathf.Abs(player.transform.position.x - questActivationObject.transform.position.x);
-                distY = Mathf.Abs(player.transform.position.y - questActivationObject.transform.position.y);
-                distZ = Mathf.Abs(player.transform.position.z - questActivationObject.transform.position.z);
-
-                if ((!qp.isQuestActive) && (distX <= 1) && (distY <= 1) && (distZ <= 1)) ;
+                if (!(qp.isQuestActive))
                 {
-                    qp.isQuestActive = true;
+                    distX = Mathf.Abs(player.transform.position.x - questActivationObject.transform.position.x);
+                    distY = Mathf.Abs(player.transform.position.y - questActivationObject.transform.position.y);
+                    distZ = Mathf.Abs(player.transform.position.z - questActivationObject.transform.position.z);
 
-                    //Debug.Log("Quest Activated");
-                    Debug.Log("player(x | y | z) == (" + distX + " | " + distY + " | " + distZ + ")");
+                    if ((distX < interactionDistance) && (distY < interactionDistance) && (distZ < interactionDistance))
+                    {
+                        qp.isQuestActive = true;
+
+                        Debug.Log("Quest Activated");
+                    }
                 }
+
                 if (qp.isQuestActive)
                 {
-                    //ready to do something with quest
+                    distX_finisher = Mathf.Abs(player.transform.position.x - questFinishObject.transform.position.x);
+                    distY_finisher = Mathf.Abs(player.transform.position.y - questFinishObject.transform.position.y);
+                    distZ_finisher = Mathf.Abs(player.transform.position.z - questFinishObject.transform.position.z);
+
+                    distX_starter = Mathf.Abs(player.transform.position.x - questActivationObject.transform.position.x);
+                    distY_starter = Mathf.Abs(player.transform.position.y - questActivationObject.transform.position.y);
+                    distZ_starter = Mathf.Abs(player.transform.position.z - questActivationObject.transform.position.z);
+
+                    if (!(goneAwayFromEmployer) && (distX_starter > interactionDistance + 5.0f) && (distY_starter > interactionDistance + 5.0f) )
+                    {
+                        goneAwayFromEmployer = true;
+                        Debug.Log("I went away from employer");
+                    }
+
+
+                    if ((distX_finisher < interactionDistance) && (distY_finisher < interactionDistance) && (distZ_finisher < interactionDistance))
+                    {
+                        wentToFinishingObject = true;
+
+                        Debug.Log("Quest Progress");
+                    }
+
+                    if((!wentToFinishingObject) && (goneAwayFromEmployer) && (distX_starter < interactionDistance) && (distY_starter < interactionDistance) && (distZ_starter < interactionDistance))
+                    {
+                        Debug.Log("You did not come to the place I've send You to. Go away, and do not come to me again. I will not give You any job in the future!");
+
+                        qp.isQuestFailure = true;
+                    }
+                    else if((wentToFinishingObject) && (distX_starter < interactionDistance) && (distY_starter < interactionDistance) && (distZ_starter < interactionDistance))
+                    {
+                        Debug.Log("Good Job! Here's your reward.");
+                    }
+
                 }
             }
         }
