fe86b8aa9bd678a410a0202da287416a6a69c596
kamortech <32922387+kamortech@users.noreply.github.com>
2020-11-24 15:18:53 +0100

    Quest System Fisrt Integration
    I've done some simple Quest System
    assumptions. I've tried to add some sample quest, but there is a problem with logical conditions like and which are not doing the job properly. I will continue to solve this problem in this week, and I will add first sample quest by the next week.

---
+++ Game/Assets/Scripts/QuestManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    struct QuestParameters
+    {
+        GameObject questActivationObject;
+
+        bool isQuestOptional;
+        bool isQuestAvailable;
+        bool isQuestReadyToActive;
+        bool isQuestActive;
+        bool isQuestSuccess;
+        bool isQuestFailure;
+    }
+
+    public GameObject[] quests;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //quests = new GameObject[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Game/Assets/Scripts/Quests/SampleQuest1.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleQuest1 : MonoBehaviour
+{
+    //must be automatic in future
+    public GameObject player;
+
+    float distX;
+    float distY;
+    float distZ;
+
+    //must consider to use connection from QuestManager's definition of this struct
+    struct QuestParameters
+    {
+        public GameObject questActivationObject;
+
+        public bool isQuestOptional;
+        public bool isQuestAvailable;
+        public bool isQuestReadyToActive;
+        public bool isQuestActive;
+        public bool isQuestSuccess;
+        public bool isQuestFailure;
+    }
+
+
+    QuestParameters qp;
+    public GameObject questActivationObject;
+
+    void Start()
+    {
+        //in future this could 'come' from QuestManager
+        qp.questActivationObject = questActivationObject;
+
+        qp.isQuestOptional = false;
+        qp.isQuestAvailable = true;
+        qp.isQuestReadyToActive = true;
+        qp.isQuestActive = false;
+        qp.isQuestSuccess = false;
+        qp.isQuestFailure = false;
+
+        if (qp.isQuestAvailable && qp.isQuestReadyToActive)
+        {
+            Debug.Log("Quest just ready to active");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!(qp.isQuestSuccess))
+        {
+            if (!(qp.isQuestFailure))
+            {
+
+                distX = Mathf.Abs(player.transform.position.x - questActivationObject.transform.position.x);
+                distY = Mathf.Abs(player.transform.position.y - questActivationObject.transform.position.y);
+                distZ = Mathf.Abs(player.transform.position.z - questActivationObject.transform.position.z);
+
+                if ((!qp.isQuestActive) && (distX <= 1) && (distY <= 1) && (distZ <= 1)) ;
+                {
+                    qp.isQuestActive = true;
+
+                    //Debug.Log("Quest Activated");
+                    Debug.Log("player(x | y | z) == (" + distX + " | " + distY + " | " + distZ + ")");
+                }
+                if (qp.isQuestActive)
+                {
+                    //ready to do something with quest
+                }
+            }
+        }
+    }
+}
