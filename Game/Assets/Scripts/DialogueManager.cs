d8beea1f2c1ce1b028186c4b759b5a11c3620560
kamortech <32922387+kamortech@users.noreply.github.com>
2020-12-08 15:56:32 +0100

    Adding quest system dialogue options and consequences + some simple character spectator
    Simple dialogue system has been added to sample quest 1, and there also is added new spectator made by using UMA 2 asset.

---
+++ Game/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject canvas;
+    public GameObject player;
+    private bool dialogueActive;
+    private int currentOption;
+    private int maxOption;
+    private Transform pointer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueActive = false;
+        currentOption = 0;
+        maxOption = 0;
+
+        pointer = canvas.transform.GetChild(0).gameObject.transform.GetChild(8);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(dialogueActive)
+        {
+            if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && (currentOption > 0))
+            {
+                currentOption--;
+                Debug.Log(currentOption);
+                movePointerUp();
+            }
+
+            if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && (currentOption < maxOption))
+            {
+                currentOption++;
+                Debug.Log(currentOption);
+                movePointerDown();
+            }
+        }
+    }
+
+    private void movePointerUp()
+    {
+        pointer.position += new Vector3(0, 40.0f, 0);
+    }
+
+    private void movePointerDown()
+    {
+        pointer.position += new Vector3(0, -40.0f, 0);
+    }
+
+    public int getDecision()
+    {
+        return currentOption;
+    }
+
+    public void setNumberOfOptions(int num)
+    {
+        maxOption = num;
+    }
+
+    public void activateDialogue()
+    {
+        dialogueActive = true;
+        canvas.transform.GetChild(0).gameObject.active = true;
+        player.GetComponent<UnityTemplateProjects.SimpleCameraController>().enabled = false;
+    }
+
+    public void deactivateDialogue()
+    {
+        dialogueActive = false;
+        canvas.transform.GetChild(0).gameObject.active = false;
+        player.GetComponent<UnityTemplateProjects.SimpleCameraController>().enabled = true;
+
+        currentOption = 0;
+        maxOption = 0;
+    }
+
+    public void setDialogueOption(int x, string txt)
+    {
+        if (maxOption + 1 == x)
+        {
+            maxOption++;
+            canvas.transform.GetChild(0).gameObject.transform.GetChild(x).GetComponent<Text>().text = txt;
+        }
+        else if (maxOption + 1 < x)
+        {
+            maxOption++;
+            canvas.transform.GetChild(0).gameObject.transform.GetChild(maxOption).GetComponent<Text>().text = txt;
+            Debug.Log("It's not allowed do declare number of dialogue option bigger than max option value plus one. It was declared as maxOption + 1 instead.");
+        }
+        else if (x < 0)
+        {
+            canvas.transform.GetChild(0).gameObject.transform.GetChild(x).GetComponent<Text>().text = txt;
+            Debug.Log("It's not allowed do declare number of dialogue option smaller than 0. It was declared as 0.");
+        }
+        else
+            canvas.transform.GetChild(0).gameObject.transform.GetChild(x).GetComponent<Text>().text = txt;
+    }
+
+    public void clearDialogueOption(int x)
+    {
+        canvas.transform.GetChild(0).gameObject.transform.GetChild(x).GetComponent<Text>().text = "";
+        //we will need to assign all options from the beginning, to prevent some option missing in beetween some others
+    }
+
+    public void clearDialogueOptions()
+    {
+        for (int i = 0; i < 8; i++)
+            canvas.transform.GetChild(0).gameObject.transform.GetChild(i).GetComponent<Text>().text = "";
+
+        currentOption = 0;
+        maxOption = 0;
+    }
+}
--- Game/Assets/Scripts/Quests/SampleQuest1.cs
+++ Game/Assets/Scripts/Quests/SampleQuest1.cs
@@ -7,6 +7,7 @@ public class SampleQuest1 : MonoBehaviour
     //must be automatic in future
     public GameObject player;
     public GameObject questFinishObject;
+    public GameObject dialogueManager;
 
     float distX;
     float distY;
@@ -43,6 +44,12 @@ public class SampleQuest1 : MonoBehaviour
     QuestParameters qp;
     public GameObject questActivationObject;
 
+    //table for storing dialogue sequences, last is always 1, if there is a 0 somewere in the middle or beginning it means, that there was some negative attitude
+    private bool[] dialogueSequences;
+
+    //variable for storin last dialogue decision
+    private int lastDecision;
+
     void Start()
     {
         //in future this could 'come' from QuestManager
@@ -63,6 +70,9 @@ public class SampleQuest1 : MonoBehaviour
         {
             Debug.Log("Quest just ready to active");
         }
+
+        //we will need to declare specific number of dialogue sequences during this quest
+        dialogueSequences = new bool[10];
     }
 
     // Update is called once per frame
@@ -83,11 +93,51 @@ public class SampleQuest1 : MonoBehaviour
                         qp.isQuestActive = true;
 
                         Debug.Log("Quest Activated");
+
+                        //activating dialogue
+                        dialogueManager.GetComponent<DialogueManager>().activateDialogue();
+
+                        //clearing all dialogue options
+                        dialogueManager.GetComponent<DialogueManager>().clearDialogueOptions();
+
+                        //setting dialogue options
+                        dialogueManager.GetComponent<DialogueManager>().setDialogueOption(0, "Nothing. I just got here.");
+                        dialogueManager.GetComponent<DialogueManager>().setDialogueOption(1, "What's your problem?");
+
+                        //starting first (0) dialogue sequence
+                        dialogueSequences[0] = true;
                     }
                 }
 
-                if (qp.isQuestActive)
+                if (qp.isQuestActive && dialogueSequences[0] && !dialogueSequences[1])
                 {
+                    if (Input.GetKeyDown(KeyCode.Return))
+                    {
+                        lastDecision = dialogueManager.GetComponent<DialogueManager>().getDecision();
+
+                        if (lastDecision == 1)
+                        {
+                            dialogueSequences[1] = false;
+                            Debug.Log("I am very dissapointed with your attitude. I will not give You any job anymore.");//here instead of Debug.Log should be coorutine for handling response monologue of interlocutor(person, wchich we are just talkink to)
+                            dialogueManager.GetComponent<DialogueManager>().deactivateDialogue();
+                        }
+                        else if (lastDecision == 0)
+                        {
+                            dialogueSequences[1] = true;
+                            Debug.Log("OK, If You find a guy who ownes me some money, please tell him that Big Beard is sending regards.");//here instead of Debug.Log should be coorutine for handling response monologue of interlocutor(person, wchich we are just talkink to)
+                        }
+
+                    }
+                }
+
+                if (qp.isQuestActive && dialogueSequences[1] && !dialogueSequences[2])
+                {
+                    //ending dialogue
+                    dialogueManager.GetComponent<DialogueManager>().deactivateDialogue();
+
+                    //clearing all dialogue options
+                    dialogueManager.GetComponent<DialogueManager>().clearDialogueOptions();
+
                     distX_finisher = Mathf.Abs(player.transform.position.x - questFinishObject.transform.position.x);
                     distY_finisher = Mathf.Abs(player.transform.position.y - questFinishObject.transform.position.y);
                     distZ_finisher = Mathf.Abs(player.transform.position.z - questFinishObject.transform.position.z);
@@ -96,7 +146,7 @@ public class SampleQuest1 : MonoBehaviour
                     distY_starter = Mathf.Abs(player.transform.position.y - questActivationObject.transform.position.y);
                     distZ_starter = Mathf.Abs(player.transform.position.z - questActivationObject.transform.position.z);
 
-                    if (!(goneAwayFromEmployer) && (distX_starter > interactionDistance + 5.0f) && (distY_starter > interactionDistance + 5.0f) )
+                    if (!(goneAwayFromEmployer) && (distX_starter > interactionDistance + 5.0f) && (distY_starter > interactionDistance + 5.0f))
                     {
                         goneAwayFromEmployer = true;
                         Debug.Log("I went away from employer");
@@ -107,18 +157,63 @@ public class SampleQuest1 : MonoBehaviour
                     {
                         wentToFinishingObject = true;
 
+                        //activating dialogue
+                        dialogueManager.GetComponent<DialogueManager>().activateDialogue();
+
+                        //clearing all dialogue options
+                        dialogueManager.GetComponent<DialogueManager>().clearDialogueOptions();
+
+                        //setting dialogue options
+                        dialogueManager.GetComponent<DialogueManager>().setDialogueOption(0, "Big Beard is sending regards. Give me what you own to him.");
+                        dialogueManager.GetComponent<DialogueManager>().setDialogueOption(1, "Hey thief, Big Beard send me to get what You've taken from him.");
+
+                        dialogueSequences[2] = true;
+
                         Debug.Log("Quest Progress");
                     }
+                }
 
-                    if((!wentToFinishingObject) && (goneAwayFromEmployer) && (distX_starter < interactionDistance) && (distY_starter < interactionDistance) && (distZ_starter < interactionDistance))
+                if (qp.isQuestActive && dialogueSequences[2] && !dialogueSequences[3])
+                {
+                    if (Input.GetKeyDown(KeyCode.Return))
                     {
-                        Debug.Log("You did not come to the place I've send You to. Go away, and do not come to me again. I will not give You any job in the future!");
+                        lastDecision = dialogueManager.GetComponent<DialogueManager>().getDecision();
 
-                        qp.isQuestFailure = true;
+                        if (lastDecision == 0)
+                        {
+                            dialogueSequences[3] = true;
+                            Debug.Log("OK, here You have his artefact. I don' need it anymore.");//here instead of Debug.Log should be coorutine for handling response monologue of interlocutor(person, wchich we are just talkink to)
+                            dialogueManager.GetComponent<DialogueManager>().deactivateDialogue();
+                        }
+                        else if (lastDecision == 1)
+                        {
+                            dialogueSequences[3] = true;
+                            Debug.Log("OK, here You have his artefact. I don' need it anymore.");//here instead of Debug.Log should be coorutine for handling response monologue of interlocutor(person, wchich we are just talkink to)
+                            Debug.Log("But, you were very rude to me, so I will remember that.");//and there sould come some reputation mark--
+                        }
+
+                        //ending dialogue
+                        dialogueManager.GetComponent<DialogueManager>().deactivateDialogue();
+
+                        //clearing all dialogue options
+                        dialogueManager.GetComponent<DialogueManager>().clearDialogueOptions();
                     }
-                    else if((wentToFinishingObject) && (distX_starter < interactionDistance) && (distY_starter < interactionDistance) && (distZ_starter < interactionDistance))
+                }
+
+                if (qp.isQuestActive && dialogueSequences[3])
+                {
+                    distX_finisher = Mathf.Abs(player.transform.position.x - questFinishObject.transform.position.x);
+                    distY_finisher = Mathf.Abs(player.transform.position.y - questFinishObject.transform.position.y);
+                    distZ_finisher = Mathf.Abs(player.transform.position.z - questFinishObject.transform.position.z);
+
+                    distX_starter = Mathf.Abs(player.transform.position.x - questActivationObject.transform.position.x);
+                    distY_starter = Mathf.Abs(player.transform.position.y - questActivationObject.transform.position.y);
+                    distZ_starter = Mathf.Abs(player.transform.position.z - questActivationObject.transform.position.z);
+
+                    if ((distX_starter < interactionDistance) && (distY_starter < interactionDistance) && (distZ_starter < interactionDistance))
                     {
                         Debug.Log("Good Job! Here's your reward.");
+                        //here should come some income for propper quest ending
                     }
 
                 }
